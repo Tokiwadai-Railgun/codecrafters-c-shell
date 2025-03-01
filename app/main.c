6e67624def05958f118edcbbe812e45a2641f32c
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-03-01 15:47:46 +0100

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
+    // Uncomment this block to pass the first stage
 
-  // Wait for user input
-  char input[100];
+    // Wait for user input
+    char input[100];
 
-  while (1) {
-    printf("$ ");
-    fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
+    char commands[3][10] = {"echo", "type", "exit"};
+    char types[3][7] = {"builtin","builtin","builtin"};
 
-    char * token = strtok(input, " ");
-    char * arguments = strtok(NULL, "\0");
+    while (1) {
+        printf("$ ");
+        fgets(input, 100, stdin);
+        input[strlen(input) - 1] = '\0';
 
-    if (strcmp(token, "echo") == 0) {
-      printf("%s\n", arguments);
-    } else if (strcmp(token, "exit") == 0) {
-      exit(0);
-    } else {
-      printf("%s: command not found\n", input);
+        char * token = strtok(input, " ");
+        char * arguments = strtok(NULL, "\0");
+
+        if (strcmp(token, "echo") == 0) {
+            printf("%s\n", arguments);
+        } else if (strcmp(token, "exit") == 0) {
+            exit(0);
+        } else if (strcmp(token, "type")  == 0) {
+            bool found = false;
+            for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++) {
+                if (strcmp(arguments, commands[i]) == 0) {
+                    printf("%s is a shell builtin\n", arguments);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                printf("%s: Command not found\n", arguments);
+            }
+        } else {
+            printf("%s: command not found\n", input);
+        }
     }
 
-
-  }
-
-
-  return 0;
+    return 0;
 }
