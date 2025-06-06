a26dfa072a2f01ca1c8898e19712c1b8347329ad
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-06-06 19:00:12 +0200

    Enhancement, put all the functions in an array

--- app/main.c
+++ app/main.c
@@ -2,34 +2,55 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Definitions of commands functions
+int echo(char arguments[]);
+int exit_function(char arguments[]);
+
+const int commandNumbers = 1;
+char *commandsNames[] = { "exit","echo" };
+int (*p[]) (char[]) = {exit_function, echo};
+
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
+    // Wait for user input
+    char input[100];
 
-  // Wait for user input
-  char input[100];
+    while (1) {
+        printf("$ ");
+        fgets(input, 100, stdin);
+        input[strlen(input) - 1] = '\0';
 
-  while (1) {
-    printf("$ ");
-    fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
+        char * token = strtok(input, " ");
+        char * arguments = strtok(NULL, "\0");
 
-    char * token = strtok(input, " ");
-    char * arguments = strtok(NULL, "\0");
 
-    if (strcmp(token, "echo") == 0) {
-      printf("%s\n", arguments);
-    } else if (strcmp(token, "exit") == 0) {
-      exit(0);
-    } else {
-      printf("%s: command not found\n", input);
+        int command = 0;
+        // Check for commands
+        for (int i = 0; i <= commandNumbers; i++) {
+            if (strcmp(token, commandsNames[i]) == 0) {
+                command = 1;
+                int success = (*p[i]) (arguments);
+                if (success != 0) {
+                    printf("Command failed with exit status : %i\n", success);
+                }
+                continue;
+            }
+        }
+    
+        if (command == 1) { continue; };
+
+        printf("%s: command not found\n", input);
     }
+    return 0;
+}
 
+int exit_function(char arguments[]) {
+    exit(0);
+}
 
-  }
-
-
-  return 0;
+int echo(char arguments[]) {
+    printf("%s\n", arguments);
+    return 0;
 }
