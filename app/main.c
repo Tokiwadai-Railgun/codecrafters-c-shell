ceb80eede31bee1c459d4d294863674a4c618969
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-06-08 14:33:53 +0200

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -5,10 +5,11 @@
 // Definitions of commands functions
 int echo(char arguments[]);
 int exit_function(char arguments[]);
+int type(char arguments[]);
 
-const int commandNumbers = 1;
-char *commandsNames[] = { "exit","echo" };
-int (*p[]) (char[]) = {exit_function, echo};
+const int commandNumbers = 3;
+char *commandsNames[] = { "exit","echo", "type" };
+int (*p[]) (char[]) = {exit_function, echo, type};
 
 int main() {
     // Flush after every printf
@@ -20,22 +21,25 @@ int main() {
     while (1) {
         printf("$ ");
         fgets(input, 100, stdin);
+
+        if (strlen(input) == 1) { continue; } // 1 because of carriage return char
+
         input[strlen(input) - 1] = '\0';
 
         char * token = strtok(input, " ");
-        char * arguments = strtok(NULL, "\0");
+        char * arguments = strtok(NULL, "");
 
 
         int command = 0;
         // Check for commands
-        for (int i = 0; i <= commandNumbers; i++) {
+        for (int i = 0; i < commandNumbers; i++) {
             if (strcmp(token, commandsNames[i]) == 0) {
                 command = 1;
                 int success = (*p[i]) (arguments);
                 if (success != 0) {
                     printf("Command failed with exit status : %i\n", success);
                 }
-                continue;
+                break;
             }
         }
     
@@ -51,6 +55,23 @@ int exit_function(char arguments[]) {
 }
 
 int echo(char arguments[]) {
+    if (arguments == NULL) return 0;
     printf("%s\n", arguments);
+    return 0;
+}
+
+int type(char arguments[]) {
+    if (arguments == NULL) {
+        printf("Invalid command\n");
+        return 0;
+    }
+    for (int i = 0; i < commandNumbers; i++) {
+        if (strcmp(&arguments[0], commandsNames[i]) == 0) {
+            printf("%s is a shell builtin\n", &arguments[0]);
+            return 0;
+        }
+    }
+
+    printf("%s: not found\n", &arguments[0]);
     return 0;
 }
